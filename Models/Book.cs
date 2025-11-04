50977fe35f338c0e0bd02866cc49245a59206feb
David Paulins <A00173358@mycambrian.ca>
2025-11-04 12:39:15 -0500

    Added all the names of the models and added code for UserBook and Book classes. Fixed some capitalization issues in nav menu

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Project_v0._1.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public int AuthorID { get; set; }
+        public int CategoryID { get; set; }
+        public int ISBN { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int PageCount { get; set; }
+        public DateTime PublishYear { get; set; }
+        public DateTime DateAdded { get; set; }
+
+    }
+}
---
+++ Models/UserBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Project_v0._1.Models
+{
+    public enum Status
+    {
+        Wishlist = 0,
+        Owned = 1,
+        Reading = 2,
+        Finished = 3
+    }
+    public class UserBook
+    {
+        public int OwnedBookID { get; set; }
+        public int UserID { get; set; }
+        public int BookID { get; set; }
+        public Status Status { get; set; } = Status.Owned;
+        public int CurrentPage { get; set; } = 0;
+        public DateTime StartDate { get; set; }  
+        public DateTime? EndDate { get; set; }
+        public DateTime DateAdded { get; set; }
+
+
+    }
+}
